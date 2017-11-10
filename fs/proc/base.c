7a58b52c7362706a25860b00c7edceabd8d9e93a
Marissa Wall <marissaw@google.com>
2017-11-10 11:09:07 -0800

    cpufreq: concurrent_policy_time by pid
    In order to model the energy used by the cpu,
    we need to expose cpu time information to
    userspace. Output the time each pid spent running
    on group of cpu's with the same policy with 1
    to num_related_cpus online and not idle.
    
    We can use this information to blame pids and
    uids for the energy they are using.
    
    For instance, if 2 cores that are a part of the
    same policy are online and active for 50ms and
    the pid is running on one of the cores, the pid
    should be blamed for 1/2 of the energy that is
    used everytime one or more cpus in the same policy
    is active. Using other proc files, the pid can
    also be blamed for the cost of keeping the entire
    cpu unsuspended and the cost of the individual
    cores it is using.
    
    b/68399339
    
    Test: cat /proc/<PID>/concurrent_policy_time
    Change-Id: I4aa7358e848c6da2c487783913f780bc6611a94d
    Signed-off-by: Marissa Wall <marissaw@google.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -3216,6 +3216,7 @@ static const struct pid_entry tgid_base_stuff[] = {
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
 	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
+	ONE("concurrent_policy_time", 0444, proc_concurrent_policy_time_show),
 #endif
 };
 
@@ -3614,6 +3615,7 @@ static const struct pid_entry tid_base_stuff[] = {
 #ifdef CONFIG_CPU_FREQ_STAT
 	ONE("time_in_state", 0444, proc_time_in_state_show),
 	ONE("concurrent_active_time", 0444, proc_concurrent_active_time_show),
+	ONE("concurrent_policy_time", 0444, proc_concurrent_policy_time_show),
 #endif
 };
 
