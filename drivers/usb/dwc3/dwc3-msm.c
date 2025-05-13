f6c5357f803d2c2aca33a76d6ea63506b913febd
Richard Raya <rdxzv.dev@gmail.com>
2025-05-13 12:32:02 -0300

    usb: dwc3: Add timeout to wakelock to 500ms
    Change-Id: I46d8d533a546c99d6fd6a234c5beede42f5ff799
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -2691,7 +2691,7 @@ static int dwc3_msm_resume(struct dwc3_msm *mdwc)
 		return 0;
 	}
 
-	pm_stay_awake(mdwc->dev);
+	pm_wakeup_event(mdwc->dev, 500);
 
 	/* Enable bus voting */
 	if (mdwc->bus_perf_client) {
